d86bbbdbfcfabd8cd6bc0b831b8a936124cb53d3
chen <tianclch@gmail.com>
2017-01-19 02:40:02 -0500

    first commit

---
+++ android/app/src/main/java/com/reactnativestarter/MainActivity.java
@@ -0,0 +1,16 @@
+package com.reactnativestarter;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     * TODO: change the name of the application
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "ReactNativeStarter";
+    }
+}
